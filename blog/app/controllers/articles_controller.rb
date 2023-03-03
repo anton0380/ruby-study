a8185935cb90981935dfd355ec4b297dc550d743
anton0380 <aa@unelma.ru>
2023-03-03 16:28:18 +0300

    correct with rubocop and modify rdoc for hello.rb

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+# Articles
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
--- lessons/hello.rb
+++ lessons/hello.rb
@@ -1,20 +1,28 @@
-##
+# frozen_string_literal: true
+
 # Class for user information
 class User
-##
-# Output information about user
-# use parameters +name+ and +profession+
-#
-# = Example for use
-#
-#   u = User('My name', 'My profession)
-#   puts u.fio
-#   puts u.profession
+  # Output information about user
+  # use parameters +name+ and +profession+
+  #
+  # ==== Examples
+  #
+  #  u = User.new('My name', 'My profession')
+  #  puts u.fio
+  #  puts u.profession
   def initialize(name, profession)
     @name = name
     @profession = profession
   end
 
+  # Get user name information
+  #
+  # ==== Examples
+  #
+  # Illustrate the behaviour of the method using examples. Indent examples:
+  #
+  #  u = User.new('My name', 'My profession')
+  #  puts u.fio
   def fio
     puts @name
   end
