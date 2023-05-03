12379877eba4462aa6ba20ab8a1a2be3c2127f0a
anton0380 <aa@unelma.ru>
2023-05-03 11:34:45 +0300

    hex: formatted output bin

--- hex/bin.rb
+++ hex/bin.rb
@@ -3,10 +3,9 @@ File.open('file.bin', 'rb') do |f|
   str = ""
   File.open('result_bin.txt', 'w') do |w|
     while buf = f.read(BLOCK_SIZE)
-      str = buf.each_byte.map{ |b| "0x#{b.to_s(16)}, " }.join
+      str = buf.each_byte.map{ |b| "0x%02X, " % b }.join
       w.write str
       w.write "\r"
-      puts str;
     end
   end
 end
