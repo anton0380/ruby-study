27b51725e4ede04612dbb8ec3499b896d683d125
anton0380 <aa@unelma.ru>
2023-05-24 23:46:17 +0300

    scripts: check video update

--- scripts/check_video.rb
+++ scripts/check_video.rb
@@ -2,4 +2,16 @@ require 'mediainfo'
 
 file_name = 'test.mp4'
 media_info = MediaInfo.from(file_name)
-p media_info
+puts media_info
+p media_info.general.filesize
+puts "full size = #{media_info.general.filesize}"
+puts "header size = #{media_info.general.headersize}"
+puts "data size = #{media_info.general.datasize}"
+puts "footer size = #{media_info.general.footersize}"
+size = media_info.general.filesize
+sum = media_info.general.headersize + media_info.general.datasize + media_info.general.footersize
+if size == sum
+  puts 'ok'
+else
+  puts 'error'
+end
