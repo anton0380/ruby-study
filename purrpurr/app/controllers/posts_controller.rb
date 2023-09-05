3db0aab4fbbfa95829a962588449fe72a5fde6e5
anton0380 <aa@unelma.ru>
2023-09-05 17:46:11 +0300

    purrpurr: create new post

--- purrpurr/app/controllers/posts_controller.rb
+++ purrpurr/app/controllers/posts_controller.rb
@@ -4,4 +4,15 @@ class PostsController < ApplicationController
 
   def show
   end
+
+  def new
+    @topics = Topic.all()
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body, :topic_id))
+    @post.save
+
+    redirect_to '/posts/' + @post.id.to_s
+  end
 end
