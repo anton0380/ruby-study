f25723c5a3adabd5b86828fe6f6a055314d83044
anton0380 <aa@unelma.ru>
2023-09-20 19:07:14 +0300

    purrpurr: pagination

--- purrpurr/app/controllers/posts_controller.rb
+++ purrpurr/app/controllers/posts_controller.rb
@@ -1,32 +1,37 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all()
+    page_token = params.has_key?(:older) ? params[:older] : params[:newer]
+    
+    paginate(page_token)
   end
 
   def topic
+    page_token = params.has_key?(:older) ? params[:older] : params[:newer]
+    
     @topic = Topic.find_by(alias: params[:topic])
-    @posts = @topic.posts
+    paginate(page_token, @topic.id)
+
     render 'index'
   end
 
   def show
     @post = Post.find(params[:id])
   end
-
+  
   def edit
-    @topics = Topic.all()
+    @topics = Topic.all() 
     @post = Post.find(params[:id])
   end
 
   def new
-    @topics = Topic.all()
+    @topics = Topic.all() 
   end
 
   def create
     @post = Post.new(post_params)
     if @post.save
       redirect_to '/posts/' + @post.id.to_s
-    else
+    else  
       @topics = Topic.all()
       render 'new'
     end
@@ -36,7 +41,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to '/posts/' + @post.id.to_s
-    else
+    else  
       @topics = Topic.all()
       render 'edit'
     end
@@ -46,5 +51,24 @@ class PostsController < ApplicationController
   def post_params
     params[:post][:topic_id] = params[:post][:topic]
     params.require(:post).permit(:title, :body, :topic_id)
+  end
+
+  def paginate(page_token, topic_id = nil)
+    pagination = Services::Pagination.new(page_token, topic_id)
+
+    if page_token.present?
+      if params.has_key?(:newer)
+        @posts = pagination.newer
+      else 
+        @posts = pagination.older
+      end  
+    else 
+      @posts = pagination.first_page
+    end
+
+    @has_newer = pagination.has_newer
+    @has_older = pagination.has_older
+
+    @page_token = pagination.construct_page_token
   end
 end
