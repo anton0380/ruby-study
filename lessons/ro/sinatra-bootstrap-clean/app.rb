18fddf34d55bd1521a78211a9e544231f5b8f47e
anton0380 <aa@unelma.ru>
2023-12-15 18:11:32 +0300

    lessons: ro visit form validation

--- lessons/ro/sinatra-bootstrap-clean/app.rb
+++ lessons/ro/sinatra-bootstrap-clean/app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
+  @error = 'asdsafghdgh'
   erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
@@ -25,5 +26,18 @@ post '/visit' do
   @datetime = params[:datetime]
   @barber = params[:barber]
   @color = params[:color]
+
+  hh = { username: 'Введите имя', phone: 'Введите телефон', datetime: 'Введите дату и время'}
+
+  # hh.each do |key, value|
+  #   if params[key] == ''
+  #     @error = hh[key]
+  #     return erb :visit
+  #   end
+  # end
+
+  @error = hh.select { |key| params[key] == '' }.values.join(', ')
+  return erb :visit unless @error == ''
+
   erb "OK! user name is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 end
