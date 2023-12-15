ea120c31cdb11d9cb25aee80493c89bf2cfc86c0
anton0380 <aa@unelma.ru>
2023-12-15 18:41:53 +0300

    barbershop: delete error on mane page

--- barbershop/app.rb
+++ barbershop/app.rb
@@ -4,7 +4,6 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  @error = 'asdsafghdgh'
   erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
@@ -28,13 +27,6 @@ post '/visit' do
   @color = params[:color]
 
   hh = { username: 'Введите имя', phone: 'Введите телефон', datetime: 'Введите дату и время'}
-
-  # hh.each do |key, value|
-  #   if params[key] == ''
-  #     @error = hh[key]
-  #     return erb :visit
-  #   end
-  # end
 
   @error = hh.select { |key| params[key] == '' }.values.join(', ')
   return erb :visit unless @error == ''
