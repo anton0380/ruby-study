9cd919693487bad893a99f526bef37f0520a3888
anton0380 <aa@unelma.ru>
2023-12-25 10:55:05 +0300

    leprosorium: post details

--- leprosorium/app.rb
+++ leprosorium/app.rb
@@ -41,3 +41,12 @@ post '/new' do
   @db.execute 'insert into Posts (content, created_date) values (?, datetime())', [content]
   erb "You typed #{content}"
 end
+
+get '/details/:post_id' do
+  post_id = params[:post_id]
+
+  results = @db.execute 'select * from Posts where id = ?', [post_id]
+  @row = results[0]
+
+  erb :details
+end
