01e850cf824e05b6f24671ec4814446f8d2426bc
anton0380 <aa@unelma.ru>
2023-12-25 15:04:06 +0300

    leprosorium: add comments

--- leprosorium/app.rb
+++ leprosorium/app.rb
@@ -21,6 +21,13 @@ configure do
     created_date DATE,
     content TEXT
   )'
+  @db.execute 'create table if not exists Comments
+  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_date DATE,
+    content TEXT,
+    post_id integer
+  )'
 end
 
 get '/' do
@@ -47,6 +54,15 @@ get '/details/:post_id' do
 
   results = @db.execute 'select * from Posts where id = ?', [post_id]
   @row = results[0]
+  @comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
 
   erb :details
+end
+
+post '/details/:post_id' do
+  post_id = params[:post_id]
+  content = params[:content]
+
+  @db.execute 'insert into Comments (content, created_date, post_id) values (?, datetime(), ?)', [content, post_id]
+  redirect to('/details/' + post_id)
 end
