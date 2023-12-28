19537c58b1acfe1c072a8bf32576679e937c5f5c
anton0380 <aa@unelma.ru>
2023-12-28 16:55:16 +0300

    pizzashop: seed products

--- pizzashop/db/schema.rb
+++ pizzashop/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_28_130530) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_28_132659) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
