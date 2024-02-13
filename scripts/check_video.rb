511886b53c411f98b549b61423f106c25dcaf332
anton0380 <aa@unelma.ru>
2024-02-14 01:30:17 +0300

    scripts: update check video

--- scripts/check_video.rb
+++ scripts/check_video.rb
@@ -1,21 +1,56 @@
 require 'mediainfo'
 require 'find'
 
-def check_file(file_name)
-  print file_name
-  media_info = MediaInfo.from(file_name)
-  data_size = media_info.general.datasize
-  video_size = media_info.video.streamsize
-  audio_size = media_info.audio.streamsize
-  surplus = data_size - video_size - audio_size
-  print " #{surplus} "
-  correct = !surplus.negative?
-  if correct
-    puts 'ok'
-  else
-    puts 'error'
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
   end
-  correct
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
+
+
+def check_file(file_name)
+  begin
+    media_info = MediaInfo.from(file_name)
+    data_size = media_info.general.datasize
+    video_size = media_info.video.streamsize
+    audio_size = media_info.audio.streamsize
+    surplus = data_size - video_size - audio_size
+    correct = !surplus.negative?
+    if !correct
+      # incorrect data size
+      puts "#{file_name} #{surplus} " + "error".red
+      # File.delete file_name
+    end
+  rescue NoMethodError
+    # error in parse file header
+    puts "#{file_name} " + "warning".yellow
+    # File.delete file_name
+  end
 end
 
 # select dir
@@ -31,7 +66,7 @@ Find.find(files_path) do |path|
     next
   else
     if File.extname(path) == '.mp4'
-      check_file(path)
+      check_file path
     end
   end
 end
