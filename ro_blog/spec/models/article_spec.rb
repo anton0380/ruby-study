778e3154570a02e2e075844407cb44ee20e6720f
anton0380 <aa@unelma.ru>
2024-04-03 18:05:48 +0300

    ro_blog: add factory bot, create factory and update article spec

--- ro_blog/spec/models/article_spec.rb
+++ ro_blog/spec/models/article_spec.rb
@@ -5,7 +5,15 @@ describe Article do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
   end
+
   describe 'associations' do
     it { should have_many :comments }
+  end
+
+  describe '#subject' do
+    it 'returns the article title' do
+      article = create(:article, title: 'Lorem Ipsum')
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end  
   end
 end
