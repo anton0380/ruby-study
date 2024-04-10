b0fd4625f9e890cdf1647ddc4c6799c0b47fd6a9
anton0380 <aa@unelma.ru>
2024-04-10 19:27:34 +0300

    lessons: ro singleton demo refactoring extract logger to separate file

--- lessons/ro/singleton_demo/app.rb
+++ lessons/ro/singleton_demo/app.rb
@@ -1,29 +1,5 @@
-class Logger
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
+require './logger'
 
-  @@x = nil
-  
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-    @@x
-  end
-
-  def self.say_something
-    puts 'haha'
-  end
-
-  def log_something wat
-    @f.puts wat
-  end
-end
-
-Logger.say_something
 Logger.instance.log_something 'blalba'
 Logger.instance.log_something 'blalba2'
-logger = Logger.new
-logger.log_something 'hey!'
-Logger.instance
+
