2143e71f7166ab8285a21e6fee8e0321760d8969
anton0380 <aa@unelma.ru>
2024-07-17 15:01:02 +0300

    algorithms: del nodes Wrong Answer

--- algorithms/del_nodes.rb
+++ algorithms/del_nodes.rb
@@ -25,14 +25,31 @@ end
 # @param {Integer[]} to_delete
 # @return {TreeNode[]}
 def del_nodes(root, to_delete)
-  root
+  root, nodes = fun(root, to_delete)
+  result = [root]
+  result += nodes unless nodes.nil?
+  result
+end
+
+def fun(root, to_delete)
+  nodes1 = []
+  nodes2 = []
+  left, nodes1 = fun(root.left, to_delete) unless root.left.nil?
+  right, nodes2 = fun(root.right, to_delete) unless root.right.nil?
+  if to_delete.include? root.val
+    d = nodes1 + nodes2
+    d += [left] unless left.nil?
+    d += [right] unless right.nil?
+    return [nil, d]
+  end
+  [TreeNode.new(root.val, left, right), nodes1 + nodes2]
 end
 
 root = TreeNode.new(
   1, TreeNode.new(2, TreeNode.new(4), TreeNode.new(5)), TreeNode.new(3, TreeNode.new(6), TreeNode.new(7))
 )
 to_delete = [3, 5]
-puts del_nodes(root, to_delete)
+puts del_nodes(root, to_delete) # Output: [[1,2,null,4],[6],[7]]
 root = TreeNode.new(1, TreeNode.new(2, TreeNode.new(4)), TreeNode.new(3))
 to_delete = [3]
-puts del_nodes(root, to_delete)
+puts del_nodes(root, to_delete) # Output: [[1,2,4]]
