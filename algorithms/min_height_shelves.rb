874b848b953b7baf7653aed062a6f633bc903eaf
anton0380 <aa@unelma.ru>
2024-07-31 11:00:59 +0300

    algorithms: min height shelves Wrong Answer 7 / 20

--- algorithms/min_height_shelves.rb
+++ algorithms/min_height_shelves.rb
@@ -2,21 +2,28 @@
 # @param {Integer} shelf_width
 # @return {Integer}
 def min_height_shelves(books, shelf_width)
-  min_height = 0
-  part_height = 0
-  part_width = 0
-  books.reverse.each do |book|
-    if shelf_width - part_width < book[0]
-      min_height += part_height
-      part_height = 0
-      part_width = 0
+  def find (books, shelf_width)
+    min_height = 0
+    part_height = 0
+    part_width = 0
+    books.each do |book|
+      if shelf_width - part_width < book[0]
+        min_height += part_height
+        part_height = 0
+        part_width = 0
+      end
+      part_width += book[0]
+      part_height = [part_height, book[1]].max
     end
-    part_width += book[0]
-    part_height = [part_height, book[1]].max
+    min_height + part_height
   end
-  min_height + part_height
+  [find(books.reverse, shelf_width), find(books, shelf_width)].min
 end
 
 books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]]
 shelf_width = 4
+puts min_height_shelves(books, shelf_width)
+
+books = [[7,3],[8,7],[2,7],[2,5]]
+shelf_width = 10
 puts min_height_shelves(books, shelf_width)
