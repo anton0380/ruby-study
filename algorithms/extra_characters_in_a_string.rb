754ba3496b366c713cdf8341f1af66551cda9bda
anton0380 <aa@unelma.ru>
2024-09-23 11:57:13 +0300

    algorithms: extra characters in a string Wrong Answer 571 / 2028

--- algorithms/extra_characters_in_a_string.rb
+++ algorithms/extra_characters_in_a_string.rb
@@ -7,7 +7,7 @@ def min_extra_char(s, dictionary)
     start = 0
     loop do
       i = s[start..].index(v)
-      break if i.nil?
+      break if i.nil? or i < start
       check[i...i+v.length] = Array.new(v.length, 0)
       start = i + 1
     end
@@ -21,3 +21,15 @@ p min_extra_char s, dictionary
 s = "sayhelloworld"
 dictionary = ["hello","world"]
 p min_extra_char s, dictionary
+
+s ="dwmodizxvvbosxxw"
+dictionary = ["ox","lb","diz","gu","v","ksv","o","nuq","r","txhe","e","wmo","cehy","tskz","ds","kzbu"]
+require 'benchmark'
+bench = Benchmark.measure {
+  p min_extra_char s, dictionary
+}
+puts bench.real
+
+s = "dwmodizxvvbosxxw"
+dictionary = ["ox","lb","diz","gu","v","ksv","o","nuq","r","txhe","e","wmo","cehy","tskz","ds","kzbu"]
+p min_extra_char s, dictionary # need 7
