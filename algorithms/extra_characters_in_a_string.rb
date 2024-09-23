4ca27373de5c6e62a670d9b6587d5416121f6ed8
anton0380 <aa@unelma.ru>
2024-09-23 12:16:07 +0300

    algorithms: extra characters in a string Wrong Answer 1964 / 2028

--- algorithms/extra_characters_in_a_string.rb
+++ algorithms/extra_characters_in_a_string.rb
@@ -7,29 +7,33 @@ def min_extra_char(s, dictionary)
     start = 0
     loop do
       i = s[start..].index(v)
-      break if i.nil? or i < start
-      check[i...i+v.length] = Array.new(v.length, 0)
-      start = i + 1
+      break if i.nil?
+      check[start+i...start+i+v.length] = Array.new(v.length, 0)
+      start += i + 1
     end
   end
   check.reduce(&:+)
 end
 
-s = "leetscode"
-dictionary = ["leet","code","leetcode"]
-p min_extra_char s, dictionary
-s = "sayhelloworld"
-dictionary = ["hello","world"]
-p min_extra_char s, dictionary
+# s = "leetscode"
+# dictionary = ["leet","code","leetcode"]
+# p min_extra_char s, dictionary
+# s = "sayhelloworld"
+# dictionary = ["hello","world"]
+# p min_extra_char s, dictionary
 
-s ="dwmodizxvvbosxxw"
-dictionary = ["ox","lb","diz","gu","v","ksv","o","nuq","r","txhe","e","wmo","cehy","tskz","ds","kzbu"]
-require 'benchmark'
-bench = Benchmark.measure {
-  p min_extra_char s, dictionary
-}
-puts bench.real
+# s ="dwmodizxvvbosxxw"
+# dictionary = ["ox","lb","diz","gu","v","ksv","o","nuq","r","txhe","e","wmo","cehy","tskz","ds","kzbu"]
+# require 'benchmark'
+# bench = Benchmark.measure {
+#   p min_extra_char s, dictionary
+# }
+# puts bench.real
 
-s = "dwmodizxvvbosxxw"
-dictionary = ["ox","lb","diz","gu","v","ksv","o","nuq","r","txhe","e","wmo","cehy","tskz","ds","kzbu"]
-p min_extra_char s, dictionary # need 7
+# s = "dwmodizxvvbosxxw"
+# dictionary = ["ox","lb","diz","gu","v","ksv","o","nuq","r","txhe","e","wmo","cehy","tskz","ds","kzbu"]
+# p min_extra_char s, dictionary # need 7
+
+s = "kevlplxozaizdhxoimmraiakbak"
+dictionary = ["yv","bmab","hv","bnsll","mra","jjqf","g","aiyzi","ip","pfctr","flr","ybbcl","biu","ke","lpl","iak","pirua","ilhqd","zdhx","fux","xaw","pdfvt","xf","t","wq","r","cgmud","aokas","xv","jf","cyys","wcaz","rvegf","ysg","xo","uwb","lw","okgk","vbmi","v","mvo","fxyx","ad","e"]
+p min_extra_char s, dictionary # need 9
