b8011b6b3d8a07c50c8ebd696db8d0303580e249
anton0380 <aa@unelma.ru>
2024-10-16 12:13:16 +0300

    ask_it: corrections

--- ask_it/app/controllers/questions_controller.rb
+++ ask_it/app/controllers/questions_controller.rb
@@ -3,11 +3,10 @@
 class QuestionsController < ApplicationController
   include QuestionsAnswers
   before_action :set_question!, only: %i[show destroy edit update]
-  before_action :fetch_tags, only: %i[new edit]
 
   def index
-    @tags = Tag.all # Tag.where(id: params[:tag_ids]) if params[:tag_ids]
-    @pagy, @questions = pagy Question.all_by_tags(params[:tag_ids])
+    @tags = Tag.where(id: params[:tag_ids]) if params[:tag_ids]
+    @pagy, @questions = pagy Question.all_by_tags(@tags)
     @questions = @questions.decorate
   end
 
@@ -54,9 +53,5 @@ class QuestionsController < ApplicationController
 
   def set_question!
     @question = Question.find params[:id]
-  end
-
-  def fetch_tags
-    @tags = Tag.all
   end
 end
--- ask_it/db/seeds.rb
+++ ask_it/db/seeds.rb
@@ -8,7 +8,14 @@
 
 # User.find_each {|u| u.send(:set_gravatar_hash); u.save}
 
+# 30.times do
+#   title = Faker::Hipster.word
+#   Tag.create title: title
+# end
+
+u = User.first
 30.times do
-  title = Faker::Hipster.word
-  Tag.create title: title
+  title = Faker::Hipster.sentence(word_count: 3)
+  body = Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 4)
+  Question.create title:, body:, user_id: u.id
 end
