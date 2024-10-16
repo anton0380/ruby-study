10500661cd117c4b5b3c503a64a4e76bf1e6b56f
anton0380 <aa@unelma.ru>
2024-10-16 18:16:50 +0300

    ask_it: politics ang guest user

--- ask_it/app/controllers/questions_controller.rb
+++ ask_it/app/controllers/questions_controller.rb
@@ -2,7 +2,10 @@
 
 class QuestionsController < ApplicationController
   include QuestionsAnswers
+  before_action :require_authentication, except: %i[show index]
   before_action :set_question!, only: %i[show destroy edit update]
+  before_action :authorize_question!
+  after_action :verify_authorized
 
   def index
     @tags = Tag.where(id: params[:tag_ids]) if params[:tag_ids]
@@ -53,5 +56,9 @@ class QuestionsController < ApplicationController
 
   def set_question!
     @question = Question.find params[:id]
+  end
+
+  def authorize_question!
+    authorize(@question || Question)
   end
 end
