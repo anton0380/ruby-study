3c821a8eb5356d02cc5eb605b7faf1f6af396308
anton0380 <aa@unelma.ru>
2024-10-17 18:48:15 +0300

    ask_it: recovery password edit

--- ask_it/app/controllers/password_resets_controller.rb
+++ ask_it/app/controllers/password_resets_controller.rb
@@ -1,5 +1,6 @@
 class PasswordResetsController < ApplicationController
   before_action :require_no_authentication
+  before_action :set_user, only: %i[edit update]
 
   def create
     @user = User.find_by email: params[:email]
@@ -12,6 +13,18 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
     
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by email: params[:user][:email],
+      password_reset_token: params[:user][:password_reset_token]
+
+    redirect_to(new_session_path, flash: { warning: t('.fail') }) unless @user&.password_reset_period_valid?
   end
 end
