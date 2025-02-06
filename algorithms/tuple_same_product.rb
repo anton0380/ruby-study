1dabebcc1ce4616d0a0b8547dc11471e4812bde1
anton0380 <aa@unelma.ru>
2025-02-06 15:35:38 +0300

    algorithms: tuple with same product Time Limit Exceeded 18 / 37 testcases passed

--- algorithms/tuple_same_product.rb
+++ algorithms/tuple_same_product.rb
@@ -1,5 +1,7 @@
 # Tuple with Same Product
 
+require 'benchmark'
+
 # @param {Integer[]} nums
 # @return {Integer}
 def tuple_same_product(nums)
@@ -40,3 +42,10 @@ p tuple_same_product nums
 # (2,5,1,10) , (2,5,10,1) , (5,2,1,10) , (5,2,10,1)
 # (2,10,4,5) , (2,10,5,4) , (10,2,4,5) , (10,2,5,4)
 # (4,5,2,10) , (4,5,10,2) , (5,4,2,10) , (5,4,10,2)
+
+nums = [195,165,57,252,52,44,42,3,128,54,150,182,120,12,17,228,26,342,75,126,110,6,66,143,77,70,65,21,55,133,221,288,104,176,255,272,360,95,200,91,7,39,135,63,323,78,209,40,171,80]
+
+bench = Benchmark.measure {
+  p tuple_same_product nums
+}
+puts bench.real # here 1.9588203409984999
